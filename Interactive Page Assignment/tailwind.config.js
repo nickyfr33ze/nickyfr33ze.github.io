511bb308f51ce33b16e5bcd25905ae7e35ffe956
Nick <43529027+nickyfr33ze@users.noreply.github.com>
2021-02-09 13:02:48 -0700

    Imported files from CSC313

---
+++ Interactive Page Assignment/tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+      colors: {
+    transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber,
+      }
+    }
+  }
---
+++ Login Page Assignment/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    future: {},
+    purge: [],
+    theme: {
+    extend: {},
+    },
+    variants: {},
+    plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+    ],
+}
+    
